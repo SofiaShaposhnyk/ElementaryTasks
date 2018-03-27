343029c8d9ea5c1deb2fbd7e5b25c2986c69299e
SofiaShaposhnyk <s.shaposhnyk@gmail.com>
2018-03-27 14:12:56 +0300

    changed func names

--- tests/test_chessBoard.py
+++ tests/test_chessBoard.py
@@ -3,17 +3,17 @@ import unittest
 
 
 class TestChessBoard(unittest.TestCase):
-    def test_valid_value(self):
+    def test_print_chess_board_valid_value(self):
         expected = "* * * * * \n * * * * *\n"
         result = ChessBoard.print_chess_board("10", "2")
         self.assertEqual(result, expected)
 
-    def test_text_value(self):
+    def test_print_chess_board_text_value(self):
         expected = "Wrong! It should be integer numbers."
         result = ChessBoard.print_chess_board("word", "input")
         self.assertEqual(result, expected)
 
-    def test_negative_value(self):
+    def test_print_chess_board_negative_value(self):
         expected = "Chessboard sizes should be positive."
         result = ChessBoard.print_chess_board("7", "-10")
         self.assertEqual(result, expected)
