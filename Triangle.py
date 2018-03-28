7c3617cdb4e72ffa624422be368eaf19c4af1ed5
SofiaShaposhnyk <s.shaposhnyk@gmail.com>
2018-03-28 15:13:16 +0300

    square method

--- Triangle.py
+++ Triangle.py
@@ -4,12 +4,9 @@ import math
 class Triangle:
     def __init__(self, name, side1, side2, side3):
         self.name = name
-        self.side1 = side1
-        self.side2 = side2
-        self.side3 = side3
-        self.square = self.get_square()
+        self.sides = (side1, side2, side3)
 
     def get_square(self):
-        half_per = (self.side1 + self.side2 + self.side3) / 2
-        return math.sqrt(half_per * (half_per - self.side1) *
-                         (half_per - self.side2) * (half_per - self.side3))
+        half_per = sum(self.sides) / 2
+        return round(math.sqrt(half_per * (half_per - self.sides[0]) *
+                         (half_per - self.sides[1]) * (half_per - self.sides[2])), 2)
