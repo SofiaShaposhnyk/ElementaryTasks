62019bff30ddf74b47718d57e9ae492cbb088b0e
SofiaShaposhnyk <s.shaposhnyk@gmail.com>
2018-04-04 03:41:45 +0300

    correct result validate func return

--- tests/test_FibSequence.py
+++ tests/test_FibSequence.py
@@ -9,25 +9,25 @@ class TestFibSequence(unittest.TestCase):
         self.assertEqual(result, expected)
 
     def test_validate_args_negative_value(self):
-        args = ["file_path", "-5", "10"]
-        expected = "Invalid arguments. Limits should be positive."
+        args = ["-5", "10"]
+        expected = False, "Invalid arguments. Limits should be positive."
         result = FibSequence.validate_args(args)
         self.assertEqual(result, expected)
 
     def test_validate_args_text_value(self):
-        args = ["file_path", "1", "end"]
-        expected = "Invalid arguments. Limits should be integer."
+        args = ["1", "end"]
+        expected = False, "Invalid arguments. Limits should be integer."
         result = FibSequence.validate_args(args)
         self.assertEqual(result, expected)
 
     def test_validate_args_invalid_value(self):
-        args = ["file_path", "1", "100", "1000"]
-        expected = "Enter two positive limits to generate a sequence."
+        args = ["1", "100", "1000"]
+        expected = False, "Enter two positive limits to generate a sequence."
         result = FibSequence.validate_args(args)
         self.assertEqual(result, expected)
 
     def test_validate_args_valid_value(self):
-        args = ["file_path", "100", "1000"]
+        args = ["100", "1000"]
         expected = (100, 1000)
         result = FibSequence.validate_args(args)
         self.assertEqual(result, expected)
