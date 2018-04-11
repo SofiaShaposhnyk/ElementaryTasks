4344751028638e08a2f63e53d258099f892ffc50
SofiaShaposhnyk <s.shaposhnyk@gmail.com>
2018-04-11 21:09:54 +0300

    add square field to Triangle

--- Triangle.py
+++ Triangle.py
@@ -5,8 +5,9 @@ class Triangle:
     def __init__(self, name, side1, side2, side3):
         self.name = name
         self.sides = (side1, side2, side3)
+        self.square = self.get_square()
 
     def get_square(self):
         half_per = sum(self.sides) / 2
         return round(math.sqrt(half_per * (half_per - self.sides[0]) *
-                         (half_per - self.sides[1]) * (half_per - self.sides[2])), 2)
+                               (half_per - self.sides[1]) * (half_per - self.sides[2])), 2)
